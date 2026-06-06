31ed008f2a9491295969d5cf9095e86378b1061d
Michael.Wang <keke060114@163.com>
2026-06-06 13:09:46 -0700

    engine_jax: package + config constants/scoring tables

---
+++ balatro_rl/engine_jax/__init__.py
@@ -0,0 +1,5 @@
+"""JAX-native, GPU-vectorizable reimplementation of the Balatro core game loop.
+
+This package is under active development. Future tasks will add:
+    CoreState, reset, step, batched_reset, batched_step
+"""
